95353d3e0123c0db5b642794abd4dd69345a08c5
João Sidney <88953258+cassianoj@users.noreply.github.com>
2023-02-18 20:34:05 +0000

    added rquirements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -34,7 +34,10 @@ def hello_world():
   return render_template('home.html', jobs=JOBS, company_name='Cassiano')
 
 
+@app.route("/api/json")
+def list_jobs():
+  return jsonify(JOBS)
+
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
-
-# @app.route("/api/json")
